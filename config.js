e95df977eca95647dec67018f8da0cd18da5f8b6
guito5407 <88219766+guito5407@users.noreply.github.com>
2024-05-05 19:53:42 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  token: process.env.TOKEN,
+  prefix: '-',
+	developers: ["940744356376248320"],
+  clientId: '1100921725673295964',
+  guildId: '899498845694742549',
+	colores: {
+		defecto: "42db9b",
+    error: "#f05454",
+    success:"#68bc6c",
+    recargar: "3261cf"
+	}
+}
