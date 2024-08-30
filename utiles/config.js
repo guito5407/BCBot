9b780d6599d3d7fc405b65db08baf28acc553523
guito5407 <88219766+guito5407@users.noreply.github.com>
2024-08-30 20:59:41 -0300

    Create config.js

---
+++ utiles/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    token: process.env.TOKEN,
+    prefix: '-',
+    owners: ["940744356376248320"/*, '345719535913140225'*/],
+    color: {
+      primary: "0099FF",
+      success: "66ba6a",
+      danger: "ef5250",
+    },
+    webhooks:[
+      {name: 'Guito', avatar: 'https://images-ext-1.discordapp.net/external/_4EXATFWebq1R7kl00QVk57izq7skPrlAbFE_LNy30U/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/940744356376248320/777d1eb23162060c55d6561c2944c2c7.png?format=webp&quality=lossless&width=408&height=408'},
+      {name: 'Cofre del tesoro', avatar: 'https://imgur.com/7Qc2ILE.png'},
+      {name: 'Minería', avatar: 'https://i.imgur.com/NIdICJK.png'},
+    ],
+    soso: [],
+    siso: []
+  }
